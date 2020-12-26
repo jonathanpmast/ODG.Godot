14b341b9358ae12c24d51074aae1f62d150f2f1a
Jonathan Mast <jonathanpmast@hotmail.com>
2020-12-26 13:55:19 -0500

    added basic movement for player

--- Source/Player.cs
+++ Source/Player.cs
@@ -3,21 +3,32 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    PlayerController _controller;
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    [Export] public int speed = 200;
 
-    
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public Vector2 velocity = new Vector2();
+
+    public void GetInput()
     {
-        
+        velocity = new Vector2();
+
+        if (Input.IsActionPressed("right"))
+            velocity.x += 1;
+
+        if (Input.IsActionPressed("left"))
+            velocity.x -= 1;
+
+        if (Input.IsActionPressed("down"))
+            velocity.y += 1;
+
+        if (Input.IsActionPressed("up"))
+            velocity.y -= 1;
+
+        velocity = velocity.Normalized() * speed;
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public override void _PhysicsProcess(float delta)
+    {
+        GetInput();
+        velocity = MoveAndSlide(velocity);
+    }
 }
