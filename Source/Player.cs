6f021b3ae4d8879cf3b6bbcab9eb495bde415314
Jonathan Mast <jonathanpmast@hotmail.com>
2020-12-26 11:57:57 -0500

    added a build task and fixed some project issues

--- Source/Player.cs
+++ Source/Player.cs
@@ -3,10 +3,12 @@ using System;
 
 public class Player : KinematicBody2D
 {
+    PlayerController _controller;
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
 
+    
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
---
+++ Source/PlayerController.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+public class PlayerController
+{
+    Player _player;
+    public PlayerController(Player player)
+    {
+        _player = player;
+    }
+}
