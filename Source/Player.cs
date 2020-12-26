4711e05a10d60d9a9111ec12ab9f70018c1ac926
Jonathan Mast <jonathanpmast@hotmail.com>
2020-12-26 15:44:44 -0500

    animated scuba steve

--- Source/Player.cs
+++ Source/Player.cs
@@ -5,30 +5,65 @@ public class Player : KinematicBody2D
 {
     [Export] public int speed = 200;
 
-    public Vector2 velocity = new Vector2();
+    public Vector2 _velocity = new Vector2();    
+    private AnimatedSprite _animatedSprite;
+    public override void _Ready()
+    {
+        _animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+    }
 
     public void GetInput()
     {
-        velocity = new Vector2();
+        _velocity = new Vector2();
 
         if (Input.IsActionPressed("right"))
-            velocity.x += 1;
-
-        if (Input.IsActionPressed("left"))
-            velocity.x -= 1;
+        {
+            _velocity.x += 1;
+        }
+        else if (Input.IsActionPressed("left"))
+        {
+            _velocity.x -= 1;            
+        }
 
         if (Input.IsActionPressed("down"))
-            velocity.y += 1;
+        {
+            _velocity.y += 1;
+        }
+        else if (Input.IsActionPressed("up"))
+        {
+            _velocity.y -= 1;            
+        }
+        _velocity = _velocity.Normalized() * speed;
+    }
 
-        if (Input.IsActionPressed("up"))
-            velocity.y -= 1;
+    private void Animate()
+    {
+        string animation = "";
+        if (_velocity.x > 0)
+            animation = "w_right";
+        else if (_velocity.x < 0)
+            animation = "w_left";
+        if (_velocity.y > 0)
+            animation = "w_down";
+        else if (_velocity.y < 0)
+            animation = "w_up";
 
-        velocity = velocity.Normalized() * speed;
+        if (_velocity == Vector2.Zero)
+            _animatedSprite.Stop();
+        else if (!_animatedSprite.IsPlaying() ||
+                 _animatedSprite.Animation != animation)
+            _animatedSprite.Play(animation);
+
+    }
+
+    public override void _Process(float delta)
+    {
+        GetInput();
     }
 
     public override void _PhysicsProcess(float delta)
     {
-        GetInput();
-        velocity = MoveAndSlide(velocity);
+        _velocity = MoveAndSlide(_velocity);
+        Animate();
     }
 }
